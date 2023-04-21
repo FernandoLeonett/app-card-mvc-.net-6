75a32a777af0073f0cfdd0e2c09ba3127f570453
fernandoleonett <fernandoleonett@gmail.com>
2023-04-21 03:03:18 +0200

    finished listt and default image

--- Controllers/CardController.cs
+++ Controllers/CardController.cs
@@ -16,7 +16,7 @@ namespace app_card.Controllers
     new Card { Title = "Shopping", Description = "Go shopping for new clothes", Id = 1, UrlImage = "~/img/shopping.jpg", DataSource = DataSource.BIRTHDAY },
     new Card { Title = "Cliff", Description = "Watch the sunset from the top of the cliff", Id = 2, UrlImage = "~/img/cliff.jpg", DataSource = DataSource.CHRISTMAS },
     new Card { Title = "Bridge", Description = "Take a walk on the bridge", Id = 3, UrlImage = "~/img/bridge.jpg", DataSource = DataSource.BIRTHDAY },
-    new Card { Title = "Bird", Description = "Take pictures of the colorful birds in the park lorem ipsusmmasasa jjasasas hhhh sss jjja kkkkas ljjjnlas fernando jose hernandez leonett es el mejor con chat gpt", Id = 4, UrlImage = "~/img/bird.jpg", DataSource = DataSource.CHRISTMAS }
+    new Card { Title = "Bird", Description = "Take pictures of the colorful birds in the park lorem ipsusmmasasa jjasasas hhhh sss jjja kkkkas ljjjnlas fernando jose hernandez leonett es el mj", Id = 4, UrlImage = "~/img/bird.jpg", DataSource = DataSource.CHRISTMAS,  }, new Card { Title = "Bird", Description = "Take pictures of the colorful birds in the park lorem ipsusmmasasa jjasasas hhhh sss jjja kkkkas ljjjnlas fernando jose hernandez leonett es el mj", Id = 4, DataSource = DataSource.CHRISTMAS }
 };
 
 
